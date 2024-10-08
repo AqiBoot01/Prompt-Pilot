9237551507fc47e90327e33ef7e3b1abd268aa58
AqiBoot01 <aqiboot007@gmail.com>
2024-10-08 16:19:20 +0500

    implemented the search functionality

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,13 +5,17 @@ import PromptCard from "./PromptCard";
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map((post) => (
-        <PromptCard
-          key={post._id}
-          post={post}
-          handleTagClick={handleTagClick}
-        />
-      ))}
+      {data.length > 0 ? (
+        data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        ))
+      ) : (
+        <p className="desc">No posts found matching your search.</p>
+      )}
     </div>
   );
 };
@@ -19,6 +23,7 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [post, setPost] = useState([]);
+  const [filteredPosts , setFilteredPosts] = useState([])
 
   useEffect(() => {
     const fetchPrompts = async () => {
@@ -27,11 +32,47 @@ const Feed = () => {
       });
       const data = await respose.json();
       setPost(data);
+      setFilteredPosts(data)
     };
     fetchPrompts();
   }, []);
 
-  const handleSearchChange = (e) => {};
+  const handleSearchChange = (e) => {
+     e.preventDefault()
+     setSearchText(e.target.value)
+  };
+
+  const handleEnterPress =(e) =>{
+    if(e.key == 'Enter') {
+      e.preventDefault()
+
+      const filteredPost = post.filter((p)=>{
+        return (
+          p.creator?.username.toLowerCase().includes(searchText.toLowerCase().trim()) ||
+          p.prompt.toLowerCase().includes(searchText.toLowerCase().trim()) ||
+          p.tag.toLowerCase() === searchText.toLowerCase().trim() 
+        )     
+      })
+
+      setFilteredPosts(filteredPost)
+      
+    }
+
+  }
+
+  const handleTagClick =(tag)=>{
+    if(tag !== ''){
+      setSearchText(tag)
+      const filteredPost = post.filter((p)=>{
+        return (
+          p.tag.toLowerCase() === tag.toLowerCase().trim() 
+        )     
+      })
+  
+      setFilteredPosts(filteredPost)
+    }
+  }
+
   return (
     <section className="feed">
       <form className="w-full flex-center">
@@ -40,11 +81,12 @@ const Feed = () => {
           placeholder="Search for a tag or a username."
           value={searchText}
           onChange={handleSearchChange}
+          onKeyDown={handleEnterPress}
           required
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={post} handleTagClick={() => {}} />
+      <PromptCardList data={filteredPosts} handleTagClick={handleTagClick} />
     </section>
   );
 };
